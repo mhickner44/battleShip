29b9dfd4d5c9becdb8f141df55764478f8fe9305
Michael <rmicu1903@gmail.com>
2023-08-30 08:57:34 -0400

    draggable ships onto placement board dom

--- src/dom.js
+++ src/dom.js
@@ -86,9 +86,38 @@ function endGame(result, winner) {
 
 }
 
+//add ship placement listeners
+function addPlacementListeners(grid) {
+  for (let i = 0; i <= 99; i++) {
+
+    grid[i].addEventListener("dragover", (event) => {
+      // prevent default to allow drop
+      event.preventDefault();
+      //set the hover for multeiple here?
+    });
+    grid[i].addEventListener("drop", (event) => {
+      // prevent default to allow drop
+      event.preventDefault();
+      let blockID = event.dataTransfer.getData("textID");
+      let infoData = event.dataTransfer.getData("data");
+      let blok = document.querySelector(blockID);
+      event.target.appendChild(blok);
+      event.target.dataset.length = infoData;
+      //change logical board 
+      //get the lenght of the incoming block
+      let lengthData = event.dataTransfer.getData("length");
 
 
-function boardSetup(){
+
+    });
+
+
+  }
+}
+
+
+
+function boardSetup() {
   const setupContainer = document.createElement("div");
   setupContainer.classList = "setupContainer";
 
@@ -98,23 +127,82 @@ function boardSetup(){
   //change orientation button
   const orientBtn = document.createElement("button");
   orientBtn.textContent = "horizontal";
-  orientBtn.classList="orientBtn";
+  orientBtn.classList = "orientBtn";
 
   //grid
   let grid = game.renderBoard();
 
+  //add the listneers to the blocks of the grid
+  addPlacementListeners(grid.childNodes);
+
+
+  let shipContainer = placementShips();
+
+  //add draggable ship elemenets
+  //do this in a loop 
+  //ship divs functino call
+
+
+  //add the ship elements here
+  // set draggable
+
+
+
+  //add ships to ship container
 
 
   //append all nodes
   setupContainer.appendChild(text);
   setupContainer.appendChild(orientBtn);
   setupContainer.appendChild(grid);
+  setupContainer.appendChild(shipContainer)
 
   return setupContainer;
 
 }
 
+function placementShips() {
+  //draggable ship container
+  const shipContainer = document.createElement("div");
+  shipContainer.classList.add("shipContainer");
+  
+  //ship type and length
+  let ships = [
+    { length: 5, shipType: 5 },
+    { length: 4, shipType: 4 },
+    { length: 3, shipType: 3 },
+    { length: 3, shipType: 2 },
+    { length: 2, shipType: 1 },]
 
+  //create the ship
+
+
+  //add the blocks of the ship
+  for (let i = 0; i < 5; i++) {
+    let placementShip = document.createElement("div");
+    placementShip.classList = "ship" + i;
+
+
+    //create the number of blocks that are needed .
+    for (let ii = 0; ii < ships[i].length; ii++) {
+      let shipBlock = document.createElement("div");
+      shipBlock.id = "innerShip"
+      placementShip.appendChild(shipBlock);
+    }
+
+
+    placementShip.setAttribute("draggable", true)
+
+    placementShip.addEventListener("dragstart", (event) => {
+      // store a ref. on the dragged elem
+      // dragged = event.target;
+      event.dataTransfer.setData("textID", "."+event.target.classList);
+      // event.dataTransfer.setData("data", event.target.dataset.length);
+    });
+    shipContainer.appendChild(placementShip)
+  }
+  return shipContainer;
+}
 
 
 export { newGame, addGridListeners, endGame, boardSetup };
